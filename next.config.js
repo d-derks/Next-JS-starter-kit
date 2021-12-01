6c7e0fbfcbf8b347def49971bddf7c921468340a
debbie.derks <debbie.derks@endava.com>
2021-12-01 12:26:18 +0100

    [BUGFIX] Next.config: add variables after @use

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ module.exports = {
     reactStrictMode: true,
     sassOptions: {
         /** make variables/mixins available to modularized scss-files */
-        additionalData: '@use "src/styles/resources/mixins/index" as *;',
+        additionalData: `
+          @use "src/styles/resources/mixins/index" as *;
+          @import "./src/styles/resources/variables.scss";
+        `,
     },
 }
