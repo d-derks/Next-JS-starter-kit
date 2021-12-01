b9c7c48207079cd09092b51a94527a65a49a5671
debbie.derks <debbie.derks@endava.com>
2021-12-01 11:51:27 +0100

    [FEATURE] Next.config: replace @import with @use path, because this needs to always be at the top

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     reactStrictMode: true,
     sassOptions: {
         /** make variables/mixins available to modularized scss-files */
-        additionalData: '@import "./src/styles/resources/variables.scss";',
+        additionalData: '@use "src/styles/resources/mixins/index" as *;',
     },
 }
