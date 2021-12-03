4e017c96fc78727f5feb7327d189fc0faf19cbb4
debbie.derks <debbie.derks@endava.com>
2021-12-03 13:07:05 +0100

    [FEATURE] Next/config: delete sass options

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    reactStrictMode: true,
-    sassOptions: {
-        /** make variables/mixins available to modularized scss-files */
-        additionalData: `
-          @use "src/styles/resources/mixins/index" as *;
-          @import "./src/styles/global/variables.scss";
-        `,
-    },
-}
+  reactStrictMode: true,
+};
