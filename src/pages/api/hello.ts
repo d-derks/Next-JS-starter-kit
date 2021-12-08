848d8974f787a11687432b17aae9a6e1000535da
debbie.derks <debbie.derks@endava.com>
2021-12-08 09:36:49 +0100

    [FEATURE] PageHeader: add height, add z-index

--- src/pages/api/hello.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import data from '../../mock/mockData.json';
-
-const fetchData = (_: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json(data)
-}
-
-export default fetchData;
