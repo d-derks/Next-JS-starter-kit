0c88142cd1ee30c5b2e6ff5a7e9eeb4face26e9b
debbie.derks <debbie.derks@endava.com>
2021-12-10 09:36:35 +0100

    [FEATURE] Typography: linter changes, add link styles

--- src/components/common/typography/Typography.tsx
+++ src/components/common/typography/Typography.tsx
@@ -1,32 +1,41 @@
-import React from 'react';
+import React from "react";
 import classNames from "classnames";
-import styles from './typography.module.scss'
+import styles from "./typography.module.scss";
 
 type TypographyProps = {
-    text: string,
-    variant: 'h1'|'h2'|'h3'|'h4'|'p'|'a',
-    size?: 'l',
-    color?: 'light'| 'dark',
-    gutterBottom?: boolean,
-    extraClass?: string,
-}
+  text: string;
+  href?: string;
+  variant: "h1" | "h2" | "h3" | "h4" | "p" | "a";
+  size?: "l";
+  color?: "light" | "dark";
+  gutterBottom?: boolean;
+  extraClass?: string;
+};
 
-const Typography = ({ text, variant, size, color = 'dark', gutterBottom = true, extraClass }: TypographyProps) => {
+const Typography = ({
+  text,
+  href,
+  variant,
+  size,
+  color = "dark",
+  gutterBottom = true,
+  extraClass,
+}: TypographyProps) => {
+  const className = classNames(styles.host, extraClass, {
+    [styles.hasGutterBottom]: gutterBottom,
+  });
 
-    const className = classNames(styles.host, extraClass, {
-        [styles.hasGutterBottom]: gutterBottom,
-    });
-
-    return React.createElement(
-        variant,
-        {
-            className,
-            'data-variant': variant,
-            'data-size': size,
-            'data-color': color
-        },
-        text,
-    );
-}
+  return React.createElement(
+    variant,
+    {
+      className,
+      href: href,
+      "data-variant": variant,
+      "data-size": size,
+      "data-color": color,
+    },
+    text
+  );
+};
 
 export default Typography;
