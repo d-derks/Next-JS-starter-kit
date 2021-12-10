98f9b17b513ee7fcf60f3d72b68a753456f31e5b
debbie.derks <debbie.derks@endava.com>
2021-12-10 09:49:50 +0100

    [FEATURE] Index: add title head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,61 @@
-import {useState} from 'react';
-import {GetStaticProps} from 'next';
-import BasePageLayout from 'src/layout/basePageLayout/BasePageLayout';
-import Link from 'next/link'
-import Date from '#components/example/date/Date'
-import {getSortedPostsData} from '../lib/examplePosts';
-import Button from 'src/components/common/button/Button';
+import { useState } from "react";
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import BasePageLayout from "src/layout/basePageLayout/BasePageLayout";
+import Link from "next/link";
+import Date from "#components/example/date/Date";
+import { getSortedPostsData } from "../lib/examplePosts";
+import Button from "src/components/common/button/Button";
 import PageSection from "../layout/pageSection/PageSection";
 import Typography from "#components/common/typography/Typography";
 
 interface HomeProps {
-    allPostsData: {
-        date: string
-        title: string
-        id: string
-    }[]
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-    const allPostsData = getSortedPostsData()
-    return {
-        props: {
-            allPostsData
-        }
-    }
-}
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
 
-const Home = ({allPostsData}: HomeProps) => {
+const Home = ({ allPostsData }: HomeProps) => {
+  // example fetching data
+  const [value, setValue] = useState({ title: "waiting for data" });
 
-// example fetching data
-    const [value, setValue] = useState({title: 'waiting for data'});
-
-    const fetchData = async event => {
-        const response = await fetch('/api/hello');
-        setValue(await response.json())
-    }
-    // end example fetching data
-    return (
-        <BasePageLayout home>
-            <PageSection>
-                <Typography
-                    variant='h2' text={value.title}
-                />
-                <ul>
-                    {allPostsData.map(({id, date, title}) => (
-                        <li className='listItem' key={id}>
-                            <Link href={`/examplePosts/${id}`}>
-                                <a>{title}</a>
-                            </Link>
-                            <Date dateString={date}/>
-                        </li>
-                    ))}
-                </ul>
-                <Button
-                    onClick={fetchData}
-                    text="Get data"
-                />
-            </PageSection>
-        </BasePageLayout>
-    )
-}
+  const fetchData = async (event) => {
+    const response = await fetch("/api/hello");
+    setValue(await response.json());
+  };
+  // end example fetching data
+  return (
+    <BasePageLayout home>
+      <Head>
+        <title>Starter kit</title>
+      </Head>
+      <PageSection>
+        <Typography variant="h2" text={value.title} />
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className="listItem" key={id}>
+              <Link href={`/examplePosts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <Date dateString={date} />
+            </li>
+          ))}
+        </ul>
+        <Button onClick={fetchData} text="Get data" />
+      </PageSection>
+    </BasePageLayout>
+  );
+};
 
 export default Home;
