1ae2019b8cf98e89110564907b39c86b3493bc18
debbie.derks <debbie.derks@endava.com>
2022-01-03 16:13:40 +0100

    [FEATURE] Grid: add component with styles

---
+++ src/components/form/Form.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Typography from "#components/common/typography/Typography";
+import styles from "#components/form/form.module.scss";
+import classNames from "classnames";
+
+type FormProps = {
+  title: string;
+  children: React.ReactNode;
+  className?: string;
+};
+
+const Form = ({ title, children, className }: FormProps) => {
+  const blockClass = classNames(styles.host, className);
+
+  return (
+    <form className={blockClass}>
+      <fieldset>
+        <Typography text={title} variant="legend" />
+        {children}
+      </fieldset>
+    </form>
+  );
+};
+
+export default Form;
