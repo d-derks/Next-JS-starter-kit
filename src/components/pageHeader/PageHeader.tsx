34720ce61dd87c7c323505dac3244f9d02312402
debbie.derks <debbie.derks@endava.com>
2022-01-07 11:55:42 +0100

    [FEATURE]  Pageheader: remove state

--- src/components/pageHeader/PageHeader.tsx
+++ src/components/pageHeader/PageHeader.tsx
@@ -1,40 +1,29 @@
-import React, {useState} from 'react';
+import React from "react";
 import classNames from "classnames";
-import styles from './pageHeader.module.scss';
+import styles from "./pageHeader.module.scss";
 import MenuButton from "#components/pageHeader/menuButton/MenuButton";
 import MenuMobile from "#components/pageHeader/menuMobile/menuMobile";
-import useViewport from '../../hooks/useViewport';
+import useViewport from "../../hooks/useViewport";
 
 type PageHeaderProps = {
-    children: React.ReactNode,
-}
+  children: React.ReactNode;
+};
 
-const PageHeader = ({children}: PageHeaderProps) => {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const blockClass = classNames(styles.host, {
-        [styles.isMenuVisible]: menuOpen
-    });
+const PageHeader = ({ children }: PageHeaderProps) => {
+  const blockClass = classNames(styles.host);
+  const { isLtLarge } = useViewport();
 
-    const { isLtLarge } = useViewport();
-
-    return (
-        <header className={blockClass}>
-            {
-                isLtLarge && (
-                    <>
-                        <MenuMobile menuOpen={menuOpen} />
-                        <MenuButton
-                            menuOpen={menuOpen}
-                            onClick={() => {
-                                setMenuOpen(!menuOpen);
-                            }}
-                        />
-                    </>
-                )
-            }
-            {children}
-        </header>
-    );
+  return (
+    <header className={blockClass}>
+      {isLtLarge && (
+        <>
+          <MenuMobile />
+          <MenuButton />
+        </>
+      )}
+      {children}
+    </header>
+  );
 };
 
 export default PageHeader;
