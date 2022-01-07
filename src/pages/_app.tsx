3306c82d2e75a64813bad29d185464128dda5a62
debbie.derks <debbie.derks@endava.com>
2022-01-07 11:51:37 +0100

    [FEATURE] App: add overmind provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
-import '#styles/index.scss';
-import 'tailwindcss/tailwind.css';
-import type { AppProps } from 'next/app'
+import "#styles/index.scss";
+import "tailwindcss/tailwind.css";
+import type { AppProps } from "next/app";
+import { createOvermind } from "overmind";
+import { Provider } from "overmind-react";
+import { config } from "../store/overmind";
+
+const overmind = createOvermind(config);
 
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+  return (
+    <Provider value={overmind}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default App
+export default App;
