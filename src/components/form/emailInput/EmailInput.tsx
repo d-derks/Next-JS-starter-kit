36e1b5537350d614182f60c15dfb70873b5be40f
debbie.derks <debbie.derks@endava.com>
2022-01-07 11:29:28 +0100

    [FEATURE] EmailInput: add component

---
+++ src/components/form/emailInput/EmailInput.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TextInput from "../textInput/TextInput";
+
+const EmailInput = (props) => {
+  return <TextInput {...props} type="email" />;
+};
+
+export default EmailInput;
