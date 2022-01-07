bf6ca8667afc571252f59166c5c0ce75dc3fa590
debbie.derks <debbie.derks@endava.com>
2022-01-07 11:12:34 +0100

    [FEATURE] Form: add onSubmit props, add inner class, addstyles

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -7,14 +7,15 @@ type FormProps = {
   title: string;
   children: React.ReactNode;
   className?: string;
+  onSubmit: any;
 };
 
-const Form = ({ title, children, className }: FormProps) => {
+const Form = ({ title, children, className, onSubmit }: FormProps) => {
   const blockClass = classNames(styles.host, className);
 
   return (
-    <form className={blockClass}>
-      <fieldset>
+    <form className={blockClass} onSubmit={onSubmit}>
+      <fieldset className={styles.inner}>
         <Typography text={title} variant="legend" />
         {children}
       </fieldset>
