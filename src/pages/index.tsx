0b4905101244605f4b47d3b2adbc09420d17239a
debbie.derks <debbie.derks@endava.com>
2022-01-07 11:52:45 +0100

    [FEATURE] Index: add tryout email error handling with overmind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,114 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
-import BasePageLayout from "src/layout/basePageLayout/BasePageLayout";
-import PageSection from "../layout/pageSection/PageSection";
+import BasePageLayout from "src/components/layout/basePageLayout/BasePageLayout";
+import PageSection from "../components/layout/pageSection/PageSection";
 import { getAllStandardNews } from "../../helpers/api-utils";
 import Intro from "#components/blocks/intro/Intro";
 import Card from "#components/common/card/Card";
 import { newsFeed } from "#types";
 import fetch from "node-fetch";
+import Grid from "#components/layout/grid/Grid";
+import Form from "#components/form/Form";
+import TextInput from "#components/form/textInput/TextInput";
+import FormRow from "#components/form/formRow/FormRow";
+import Button from "#components/common/button/Button";
+import { useRef } from "react";
+import EmailInput from "#components/form/emailInput/EmailInput";
+import { useOvermindActions, useOvermindState } from "../store/overmind";
+import { saveFormDetails } from "../helpers/saveFormDetails";
+
+const registerFormProps = {
+  name: {
+    label: "name",
+    placeholder: "input placeholder",
+  },
+  email: {
+    label: "e-mail address",
+    placeholder: "input placeholder",
+  },
+};
 
 const Home = ({ data, news }) => {
+  const emailRef = useRef(null);
+  const nameRef = useRef(null);
+
+  const {
+    registerEmail: { name, email },
+  } = useOvermindState();
+  const {
+    registerEmail: {
+      toggleEmailError,
+      toggleNameError,
+      handleFormErrorMessage,
+    },
+  } = useOvermindActions();
+
+  const register = () => {
+    const enteredName = nameRef.current.value;
+    const enteredEmail = emailRef.current.value;
+
+    saveFormDetails(event, enteredEmail, enteredName);
+    validateEmail(enteredEmail);
+    validateName(enteredName);
+    handleFormErrorMessage();
+  };
+  const validateName = (enteredName) => {
+    if (!enteredName || enteredName.trim() === "") {
+      toggleNameError(true);
+      return true;
+    } else {
+      toggleNameError(false);
+      return false;
+    }
+  };
+
+  const validateEmail = (enteredEmail) => {
+    if (!enteredEmail || !enteredEmail.includes("@")) {
+      toggleEmailError(true);
+      return true;
+    } else {
+      toggleNameError(false);
+      return false;
+    }
+  };
+
   return (
     <BasePageLayout home>
-        <Head>
-            <title>Starter kit</title>
-        </Head>
+      <Head>
+        <title>Starter kit</title>
+      </Head>
       <PageSection>
-        <Intro title={data.title} leadin={data.leadin} />
-        {news?.map((feed: newsFeed) => (
-          <Card
-            key={feed.id}
-            title={feed.title}
-            href={`/examples/news/${feed.id}`}
-            images={feed.images}
-          />
-        ))}
+        <Intro {...data} />
+        <Grid>
+          {news?.map((feed: newsFeed) => (
+            <Card key={feed.id} href={`/examples/news/${feed.id}`} {...feed} />
+          ))}
+        </Grid>
+      </PageSection>
+      <PageSection>
+        <Form title="Register email" onSubmit={register}>
+          <FormRow>
+            <EmailInput
+              labelFloating
+              variant="outlined"
+              id={Math.random().toString()}
+              {...registerFormProps.email}
+              inputRef={emailRef}
+              type="email"
+              error={email.message}
+            />
+            <TextInput
+              labelFloating
+              variant="outlined"
+              id={Math.random().toString()}
+              {...registerFormProps.name}
+              inputRef={nameRef}
+              type="text"
+              error={name.message}
+            />
+          </FormRow>
+          <Button text="Save emailaddress" type="submit" />
+        </Form>
       </PageSection>
     </BasePageLayout>
   );
