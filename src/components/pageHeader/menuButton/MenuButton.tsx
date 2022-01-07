a0848f41b35c71b06e03b2f30cab2d65ffbeb1d9
debbie.derks <debbie.derks@endava.com>
2022-01-07 11:54:18 +0100

    [FEATURE]  MenuButton: remove props menuOpen and onClick, add state from store

--- src/components/pageHeader/menuButton/MenuButton.tsx
+++ src/components/pageHeader/menuButton/MenuButton.tsx
@@ -1,31 +1,38 @@
-import React from 'react';
-import styles from './menuButton.module.scss'
+import React from "react";
+import styles from "./menuButton.module.scss";
 import classNames from "classnames";
+import { useOvermindActions, useOvermindState } from "../../../store/overmind";
 
 type MenuButtonProps = {
-    menuOpen?: boolean,
-    onClick?: (event: React.MouseEvent) => void;
-    className?: string,
-}
+  className?: string;
+};
 
-const MenuButton = ({menuOpen = false, onClick, className}: MenuButtonProps) => {
-    const blockClass = classNames(styles.host, className);
+const MenuButton = ({ className }: MenuButtonProps) => {
+  const blockClass = classNames(styles.host, className);
+  const {
+    app: { mainMenuOpen },
+  } = useOvermindState();
 
-    return (
-        <button
-            className={blockClass}
-            type="button"
-            title='toggle menu'
-            aria-expanded={menuOpen ? 'true' : 'false'}
-            onClick={onClick}
-        >
-          <span className={`${styles.inner} menu-button`}>
-            <span className={`${styles.text} menu-button`}>
-              Press for menu
-            </span>
-          </span>
-        </button>
-    );
+  const {
+    app: { toggleMainMenu },
+  } = useOvermindActions();
+
+  const handleMenuButtonClick = () => {
+    toggleMainMenu();
+  };
+  return (
+    <button
+      className={blockClass}
+      type="button"
+      title="toggle menu"
+      aria-expanded={mainMenuOpen ? "true" : "false"}
+      onClick={handleMenuButtonClick}
+    >
+      <span className={`${styles.inner} menu-button`}>
+        <span className={`${styles.text} menu-button`}>Press for menu</span>
+      </span>
+    </button>
+  );
 };
 
 export default MenuButton;
