d221dd847cafd2abb7e969c548e9726445422f02
debbie.derks <debbie.derks@endava.com>
2022-01-12 13:18:02 +0100

    [FEATURE]  EmailInput: add optional children

--- src/components/form/emailInput/EmailInput.tsx
+++ src/components/form/emailInput/EmailInput.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import TextInput from "../textInput/TextInput";
 
 const EmailInput = (props) => {
-  return <TextInput {...props} type="email" />;
+  return (
+    <TextInput {...props} type="email">
+      {props.children}
+    </TextInput>
+  );
 };
 
 export default EmailInput;
